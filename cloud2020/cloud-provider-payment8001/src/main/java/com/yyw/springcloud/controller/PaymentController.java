52a02eeb59027ee2e9df0aaf8e5dad0c0d3e7bac
yyw <1471835341@qq.com>
2021-08-07 11:20:33 +0800

    构建

--- cloud2020/cloud-provider-payment8001/src/main/java/com/yyw/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/yyw/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.yyw.springcloud.entities.CommonResult;
 import com.yyw.springcloud.entities.Payment;
 import com.yyw.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- cloud2020/cloud-provider-payment8001/src/main/java/com/yyw/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/yyw/springcloud/service/impl/PaymentServiceImpl.java
@@ -4,6 +4,7 @@ import com.yyw.springcloud.dao.PaymentDao;
 import com.yyw.springcloud.entities.Payment;
 import com.yyw.springcloud.service.PaymentService;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
