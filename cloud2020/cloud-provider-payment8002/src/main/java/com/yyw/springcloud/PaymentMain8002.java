30163faadbd1a8da31fdf09879d5c7d0e0619a4d
yyw <1471835341@qq.com>
2021-08-10 11:17:57 +0800

    集群

--- cloud2020/cloud-provider-payment8002/src/main/java/com/yyw/springcloud/PaymentMain8002.java
+++ cloud2020/cloud-provider-payment8002/src/main/java/com/yyw/springcloud/PaymentMain8002.java
@@ -3,10 +3,12 @@ package com.yyw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain8002 {
 
         public static void main(String[] args) {
