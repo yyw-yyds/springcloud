e664bc0e37bd4b0be3eb920f9cd85279ce9238ab
yyw <1471835341@qq.com>
2021-08-12 19:01:18 +0800

    集群

---
+++ cloud2020/cloud-consumer-feign-order80/src/main/java/com/yyw/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,34 @@
+package com.yyw.springcloud.controller;
+
+
+import com.yyw.springcloud.entities.CommonResult;
+import com.yyw.springcloud.entities.Payment;
+import com.yyw.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment>  getPaymentById(@PathVariable("id") Long id){
+
+        return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+
+        //openfeign-ribbon,客户端默认等待一秒钟
+        return paymentFeignService.paymentFeignTimeout();
+    }
+}
---
+++ cloud2020/cloud-consumer-feign-order80/src/main/java/com/yyw/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,22 @@
+package com.yyw.springcloud.service;
+
+
+import com.yyw.springcloud.entities.CommonResult;
+import com.yyw.springcloud.entities.Payment;
+import feign.Param;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
+
+}
