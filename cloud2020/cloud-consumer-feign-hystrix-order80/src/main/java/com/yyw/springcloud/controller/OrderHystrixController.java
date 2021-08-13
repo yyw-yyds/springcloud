caf1e388280defdc0ce338fb6ba10dc217bf2330
yyw <1471835341@qq.com>
2021-08-13 17:34:13 +0800

    集群

---
+++ cloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/yyw/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,33 @@
+package com.yyw.springcloud.controller;
+
+import com.yyw.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderHystrixController {
+
+
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return  result;
+    }
+
+
+}
