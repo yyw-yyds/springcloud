cf32ad782a8f6729a4134ba688612920eaf1d11e
yyw <1471835341@qq.com>
2021-08-17 17:20:54 +0800

    集群

---
+++ cloud2020/cloud-gateway-gateway9527/src/test/java/com/yyw/test/ZonedDateTimeDemo.java
@@ -0,0 +1,11 @@
+package com.yyw.test;
+
+import java.time.ZonedDateTime;
+
+public class ZonedDateTimeDemo {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
